d653041a4f215bea86f46bc106fe1075318f00d9
MarianEmilian <marianemilian99@gmail.com>
2020-06-23 16:27:20 +0300

    Update account.js

--- Frontend/javascript/account.js
+++ Frontend/javascript/account.js
@@ -1,80 +1,80 @@
-// [].forEach.call(document.getElementsByClassName('tags-input-account'), function(el) {
-//     let hiddenInput = document.createElement('input'),
-//         mainInput = document.createElement('input'),
-//         tags = [];
+[].forEach.call(document.getElementsByClassName('tags-input-account'), function(el) {
+    let hiddenInput = document.createElement('input'),
+        mainInput = document.createElement('input'),
+        tags = [];
 
-//     hiddenInput.setAttribute('type', 'hidden');
-//     hiddenInput.setAttribute('name', el.getAttribute('data-name'));
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', el.getAttribute('data-name'));
 
-//     var newLabel = document.createElement("label");
-//     newLabel.setAttribute('for','main-input')
-//     mainInput.setAttribute('type', 'text');
-//     mainInput.setAttribute('id','main-input');
-//     mainInput.classList.add('main-input');
-//     mainInput.addEventListener('input', function() {
-//         let enteredTags = mainInput.value.split(',');
-//         if (enteredTags.length > 1) {
-//             enteredTags.forEach(function(t) {
-//                 let filteredTag = filterTag(t);
-//                 if (filteredTag.length > 0)
-//                     addTag(filteredTag);
-//             });
-//             mainInput.value = '';
-//         }
-//     });
+    var newLabel = document.createElement("label");
+    newLabel.setAttribute('for','main-input')
+    mainInput.setAttribute('type', 'text');
+    mainInput.setAttribute('id','main-input');
+    mainInput.classList.add('main-input');
+    mainInput.addEventListener('input', function() {
+        let enteredTags = mainInput.value.split(',');
+        if (enteredTags.length > 1) {
+            enteredTags.forEach(function(t) {
+                let filteredTag = filterTag(t);
+                if (filteredTag.length > 0)
+                    addTag(filteredTag);
+            });
+            mainInput.value = '';
+        }
+    });
 
-//     mainInput.addEventListener('keydown', function(e) {
-//         let keyCode = e.which || e.keyCode;
-//         if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
-//             removeTag(tags.length - 1);
-//         }
-//     });
+    mainInput.addEventListener('keydown', function(e) {
+        let keyCode = e.which || e.keyCode;
+        if (keyCode === 8 && mainInput.value.length === 0 && tags.length > 0) {
+            removeTag(tags.length - 1);
+        }
+    });
 
-//     newLabel.appendChild(mainInput);
-//     el.appendChild(newLabel);
-//     el.appendChild(hiddenInput);
+    newLabel.appendChild(mainInput);
+    el.appendChild(newLabel);
+    el.appendChild(hiddenInput);
 
-//     addTag('netflix!');
+    addTag('netflix!');
 
-//     function addTag(text) {
-//         let tag = {
-//             text: text,
-//             element: document.createElement('span'),
-//         };
+    function addTag(text) {
+        let tag = {
+            text: text,
+            element: document.createElement('span'),
+        };
 
-//         tag.element.classList.add('tag');
-//         tag.element.textContent = tag.text;
+        tag.element.classList.add('tag');
+        tag.element.textContent = tag.text;
 
-//         let closeBtn = document.createElement('span');
-//         closeBtn.classList.add('close');
-//         closeBtn.addEventListener('click', function() {
-//             removeTag(tags.indexOf(tag));
-//         });
-//         tag.element.appendChild(closeBtn);
+        let closeBtn = document.createElement('span');
+        closeBtn.classList.add('close');
+        closeBtn.addEventListener('click', function() {
+            removeTag(tags.indexOf(tag));
+        });
+        tag.element.appendChild(closeBtn);
 
-//         tags.push(tag);
+        tags.push(tag);
 
-//         el.insertBefore(tag.element, mainInput);
+        el.insertBefore(tag.element, mainInput);
 
-//         refreshTags();
-//     }
+        refreshTags();
+    }
 
-//     function removeTag(index) {
-//         let tag = tags[index];
-//         tags.splice(index, 1);
-//         el.removeChild(tag.element);
-//         refreshTags();
-//     }
+    function removeTag(index) {
+        let tag = tags[index];
+        tags.splice(index, 1);
+        el.removeChild(tag.element);
+        refreshTags();
+    }
 
-//     function refreshTags() {
-//         let tagsList = [];
-//         tags.forEach(function(t) {
-//             tagsList.push(t.text);
-//         });
-//         hiddenInput.value = tagsList.join(',');
-//     }
+    function refreshTags() {
+        let tagsList = [];
+        tags.forEach(function(t) {
+            tagsList.push(t.text);
+        });
+        hiddenInput.value = tagsList.join(',');
+    }
 
-//     function filterTag(tag) {
-//         return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
-//     }
-// });
+    function filterTag(tag) {
+        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+    }
+});
